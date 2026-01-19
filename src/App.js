f8c5e502cc37dc769a5b357f08c47c9720945509
aniledulakanti24 <21d41a6921@gmail.com>
2026-01-19 06:02:02 +0000

    Counter Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import Welcome from './components/Welcome'
+
+// const App = () => {
+//   return <Welcome name="Anil" />
+// }
+// export default App
+
+import Counter from './components/Counter'
+
+const App = () => {
+  return <Counter />
+}
+
+export default App
